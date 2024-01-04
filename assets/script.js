f321889380fcb11ab375cfabec7d4c0ec203ccb8
Cooper Griffin <coopergriffin@cmail.carleton.ca>
2024-01-03 19:10:46 -0500

    Added first API call to JS
    Added first call to open movie database that searches and returns a list of movies

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,11 @@
 // assets/js/script.js
 document.addEventListener('DOMContentLoaded', function () {
     // Your JavaScript code goes here
+    const apiKey = '94dce9c5';
+    const searchTerm = 'Inception';
+
+    fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${searchTerm}`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
 });
