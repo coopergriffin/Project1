ff70ace7cbdf175456073ceff23998526cbf92b8
Cooper Griffin <coopergriffin@cmail.carleton.ca>
2024-01-08 17:39:33 -0500

    Added Comments to All Files
    Added more comments and descriptions throughout all the files in the project

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,18 @@
 //Cooper Griffin
+//Javascript for Flick Pick Game 
 
+
+//API Keys
 const apiKeyMovieDB = '7bb9f3fb04d2e6f4faa3c20430fbb78d';
 const apiKeyOpenDB = '94dce9c5';
 
 
+//Event listener when DOM is loaded 
 document.addEventListener('DOMContentLoaded', async function () {
     // Your JavaScript code goes here
     let currentHighScore = localStorage.getItem('highScore') || 0;
     document.getElementById('highScore').textContent = currentHighScore;
+
     let isFirstMovieOlder = false;
     let userScore = 0;
     let firstMovieTitle = "";
@@ -29,18 +34,25 @@ document.addEventListener('DOMContentLoaded', async function () {
 
     // Function to update the image source
     const updateImageSource = async () => {
+
+        //Adds random movie poster to img element 
         const imageElement = document.getElementById("firstImage");
         const movie = await getRandomMovie();
         const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
         imageElement.src = imageUrl;
 
+
+        //Adds random movie poster to second img element 
         const imageElement1 = document.getElementById("secondImage");
         const movie1 = await getRandomMovie();
         const imageUrl1 = `https://image.tmdb.org/t/p/w500${movie1.poster_path}`;
         imageElement1.src = imageUrl1;
 
+        //Sets var isFirstMoiveOlder to true or false 
         isFirstMovieOlder = movie.release_date <= movie1.release_date;
         console.log(isFirstMovieOlder);
+
+        //checks if movies are the same and recalls the function if it is. 
         if (movie.id === movie1.id) {
             console.log("Same movies, calling updateImage again.");
             await updateImageSource();
@@ -59,11 +71,12 @@ document.addEventListener('DOMContentLoaded', async function () {
         scoreParagraph.textContent = `${userScore}`;
     };
 
+
+    //Event handler function for the firstButton 
     const firstButtonHandler = function () {
         console.log("First Button Pressed");
         if (isFirstMovieOlder) {
             userScore += 1;
-            exampleCorrectAnswer();
             openModal('Correct!', `Great job! Your answer is correct. ${firstMovieTitle} came out in ${firstMovieDate}, and ${secondMovieTitle} came out in ${secondMovieDate} `);
         }else{
             openModal('Wrong', `You chose ${firstMovieTitle}. But ${firstMovieTitle} came out in ${firstMovieDate}, and ${secondMovieTitle} came out in ${secondMovieDate}. You end your game with a score of ${userScore} `);   
@@ -79,11 +92,11 @@ document.addEventListener('DOMContentLoaded', async function () {
         updateScoreParagraph();
     };
 
+    //Event handler function for secondButton 
     const secondButtonHandler = function () {
         console.log("Second Button Pressed");
         if (!isFirstMovieOlder) {
             userScore += 1;
-            //exampleCorrectAnswer();
             openModal('Correct!', `Great job! Your answer is correct. ${firstMovieTitle} came out in ${firstMovieDate}, and ${secondMovieTitle} came out in ${secondMovieDate} `);
         }else{
             openModal('Wrong', `You chose ${secondMovieTitle}. But ${firstMovieTitle} came out in ${firstMovieDate}, and ${secondMovieTitle} came out in ${secondMovieDate}. You end your game with a score of ${userScore}`);
@@ -98,7 +111,7 @@ document.addEventListener('DOMContentLoaded', async function () {
     };
 
 
-
+    //Event handler function for the startButton 
     startButton.addEventListener('click', function () {
         // Remove previous event listeners
         firstButton.removeEventListener('click', firstButtonHandler);
@@ -117,6 +130,9 @@ document.addEventListener('DOMContentLoaded', async function () {
     });
 });
 
+
+//Function for Modal Functionality. Connects to HTML elements 
+//Open Modal Function
 const openModal = (title, message) => {
     const modal = document.getElementById('resultModal');
     const modalTitle = document.getElementById('modalTitle');
@@ -128,17 +144,10 @@ const openModal = (title, message) => {
     modal.classList.add('is-active');
 };
 
+//Close Modal Function 
 const closeModal = () => {
     console.log("closeModal function called");
     const modal = document.getElementById('resultModal');
     modal.classList.remove('is-active');
 };
 
-// Example of using the modal
-const exampleCorrectAnswer = () => {
-    openModal('Correct!', 'Great job! Your answer is correct.');
-};
-
-const exampleWrongAnswer = () => {
-    openModal('Wrong!', 'Oops! Your answer is incorrect. Try again.');
-};
