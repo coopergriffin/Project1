a18e18f151b577c7e2e62cec44d11eac84bc673b
Cooper Griffin <coopergriffin@cmail.carleton.ca>
2024-01-08 11:30:57 -0500

    Updated JS
    Fixed scoring bug.

--- assets/script.js
+++ assets/script.js
@@ -9,85 +9,72 @@ document.addEventListener('DOMContentLoaded', async function () {
     let isFirstMovieOlder = false;
     let userScore = 0;
     let highScore = 0;
-    
 
-    // Fetch movie list from OpenDB API
-    try {
-        const response = await fetch(`http://www.omdbapi.com/?apikey=${apiKeyOpenDB}&s="inception"`);
+    // Function to fetch a random movie from TMDb
+    const getRandomMovie = async () => {
+        const response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKeyMovieDB}`);
         const data = await response.json();
-        console.log(data);
-    } catch (error) {
-        console.error('Error:', error);
-    }
 
-    startButton.addEventListener('click', function () {
-        // This function will be executed when the start button is clicked
-        userScore = 0;
-        console.log('Start button clicked!');
-        const getRandomMovie = async () => {
-            const response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKeyMovieDB}`);
-            const data = await response.json();
+        // Get a random movie from the top-rated list
+        const randomIndex = Math.floor(Math.random() * data.results.length);
+        const randomMovie = data.results[randomIndex];
+        return randomMovie;
+    };
 
-            // Get a random movie from the top-rated list
-            const randomIndex = Math.floor(Math.random() * data.results.length);
-            const randomMovie = data.results[randomIndex];
-            return randomMovie;
-        };
-        
-          // Function to update the image source
-        const updateImageSource = async () => {
-            const imageElement = document.getElementById("firstImage");
-            const movie = await getRandomMovie();
-            const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-            imageElement.src = imageUrl;
+    // Function to update the image source
+    const updateImageSource = async () => {
+        const imageElement = document.getElementById("firstImage");
+        const movie = await getRandomMovie();
+        const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+        imageElement.src = imageUrl;
 
-            const imageElement1 = document.getElementById("secondImage");
-            const movie1 = await getRandomMovie();
-            const imageUrl1 = `https://image.tmdb.org/t/p/w500${movie1.poster_path}`;
-            imageElement1.src = imageUrl1;
+        const imageElement1 = document.getElementById("secondImage");
+        const movie1 = await getRandomMovie();
+        const imageUrl1 = `https://image.tmdb.org/t/p/w500${movie1.poster_path}`;
+        imageElement1.src = imageUrl1;
 
-            isFirstMovieOlder = movie.release_date < movie1.release_date;
-            console.log(isFirstMovieOlder);
-        };
-        
-        // Change Image functions
-        window.changeImage = async () => {
-            await updateImageSource();
-        };
+        isFirstMovieOlder = movie.release_date < movie1.release_date;
+        console.log(isFirstMovieOlder);
+    };
 
-        const updateScoreParagraph = () => {
-            const scoreParagraph = document.getElementById('score');
-            scoreParagraph.textContent = `${userScore}`;
-        };
-        
-       
-        
-          // Initial setup
+    // Function to update the score paragraph
+    const updateScoreParagraph = () => {
+        const scoreParagraph = document.getElementById('score');
+        scoreParagraph.textContent = `${userScore}`;
+    };
+
+    const firstButtonHandler = function () {
+        console.log("First Button Pressed");
+        if (isFirstMovieOlder) {
+            userScore += 1;
+        }
         updateImageSource();
         updateScoreParagraph();
+    };
 
-        firstButton.addEventListener('click', function () {
-            console.log("First Button Pressed");
-            if(isFirstMovieOlder){
-                userScore += 1;
-                updateImageSource();
-                updateScoreParagraph();
-            }else{
-                updateImageSource();
-            }
-        });
+    const secondButtonHandler = function () {
+        console.log("Second Button Pressed");
+        if (!isFirstMovieOlder) {
+            userScore += 1;
+        }
+        updateImageSource();
+        updateScoreParagraph();
+    };
 
-        secondButton.addEventListener('click', function () {
-            console.log("Second Button Pressed");
-            if(!isFirstMovieOlder){
-                userScore += 1;
-                updateImageSource();
-                updateScoreParagraph();
-            }else{
-                updateImageSource();
-            }
-        });
-        
-        // You can add more code here to start your game or perform other actions
+    startButton.addEventListener('click', function () {
+        // Remove previous event listeners
+        firstButton.removeEventListener('click', firstButtonHandler);
+        secondButton.removeEventListener('click', secondButtonHandler);
+
+        // Add new event listeners
+        firstButton.addEventListener('click', firstButtonHandler);
+        secondButton.addEventListener('click', secondButtonHandler);
+
+        // Reset the user score
+        userScore = 0;
+
+        console.log('Start button clicked!');
+        updateImageSource();
+        updateScoreParagraph();
     });
 });
